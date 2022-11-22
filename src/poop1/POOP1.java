5e44691178e78da4f94b27d0d57e57fe805e5edf
ZoeCarmona <90164780+ZoeCarmona@users.noreply.github.com>
2022-11-21 20:26:11 -0600

    Práctica 1 Entorno y lenguaje de programación.

---
+++ src/poop1/POOP1.java
@@ -0,0 +1,24 @@
+package poop1;
+/**
+ * @author Carmona Ayala Mariana Zoe
+ */
+public class POOP1 {
+    public static void main(String[] args) {
+        /**
+         * Imprimimos datos personales
+         */
+        System.out.println("Hola Mundo desde POO grupo 1");
+        System.out.println("Carmona Ayala Mariana Zoe");
+        System.out.println("Edad: 19");
+        System.out.println("Maquina: 17 Bolivia");
+        System.out.println("Numero de cuenta: 319018129");
+        System.out.println("Materias:");
+        System.out.println("    1.-Programacion Orientada a Objetos");
+        System.out.println("    2.-Estructura de datos y algoritmos II");
+        System.out.println("    3.-Calculo Vectorial");
+        System.out.println("    4.-Probabilidad");
+        System.out.println("    5.-Ecuaciones Diferenciales");
+        
+    }
+    
+}
